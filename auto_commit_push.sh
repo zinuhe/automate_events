80694f118d889e972da8d6beb07fe6b929797e85
Jimmy <11039182+zinuhe@users.noreply.github.com>
2021-07-16 15:31:48 -0400

    Adding commands and colors

--- auto_commit_push.sh
+++ auto_commit_push.sh
@@ -6,5 +6,13 @@
 #fswatch -0 <<file>> | xargs -0 -n 1 bash <<path/to/auto_commit_push.sh>>
 #fswatch -0 * | xargs -0 -n 1 bash auto_commit_push.sh
 
+NOCOLOR='\033[0m'
+YELLOW='\033[1;33m'
+
+clear
+echo -e "${YELLOW}"
+pwd
+echo -e "${NOCOLOR}"
+
 git commit -m "auto commit" $1
 git push
